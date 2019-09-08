80607a87a9b24d63484748300f26f2cc0a6a0bb0
Pusenlin <934841028@qq.com>
2019-09-08 22:09:38 +0800

    35. 反转链表

---
+++ src/acwing/Linked_list/ReverseList.java
@@ -0,0 +1,31 @@
+package acwing.Linked_list;
+
+/**
+ * @author psl
+ * @date 2019/9/8
+ *          35. 反转链表
+ * 定义一个函数，输入一个链表的头结点，反转该链表并输出反转后链表的头结点。
+ * 样例
+ * 输入:1->2->3->4->5->NULL
+ * 输出:5->4->3->2->1->NULL
+ *             解题思路：
+ *
+ */
+public class ReverseList {
+    public ListNode_4 reverseList(ListNode_4 head) {
+        ListNode_4 pre = null;
+        ListNode_4 cur = head;
+        while(cur != null){
+            ListNode_4 next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
+ class ListNode_4 {
+     int val;
+     ListNode_4 next;
+     ListNode_4(int x) { val = x; }
+ }
