2b7d7b332cfaa961e65e372edef92e928b3757b3
Pusenlin <934841028@qq.com>
2019-09-08 22:12:47 +0800

    35. 反转链表

--- src/acwing/Linked_list/ReverseList.java
+++ src/acwing/Linked_list/ReverseList.java
@@ -9,6 +9,9 @@ package acwing.Linked_list;
  * 输入:1->2->3->4->5->NULL
  * 输出:5->4->3->2->1->NULL
  *             解题思路：
+ *             采用双指针法。
+ *             
+ *             时间复杂度：O(n)
  *
  */
 public class ReverseList {
